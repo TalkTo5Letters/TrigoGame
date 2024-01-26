c4c9c937ed1ddd5690c0f5276e54da90c2c4dd36
MyAngelShibuyaKanon <99974252+MyAngelShibuyaKanon@users.noreply.github.com>
2024-01-26 10:27:30 +0800

    Added sprite, character animation and camera
    Its 3 am and im suffering

--- scenes/levels/PlayerMovement.gd
+++ scenes/levels/PlayerMovement.gd
@@ -1,31 +1,32 @@
 extends Node2D
+@onready var _animated_sprite = $"../CharacterBody2D/AnimatedSprite2D"
 
 var input: String
 @onready var Player = $".." #Sets parent's name to Player
 
 func _process(delta): 
 	var velocity: float = 250 * delta #4 pixels per second
-	input = getInputAndAction(velocity, input) #Calls Input function which calls action funciton, stores last input
-	
-func getInputAndAction(velocity, input) -> String:
-	if Input.is_action_pressed(input): #check if old input is still pressed, if so, calls action immediately
-		action(velocity, input)
+	input = getInputAndAction(velocity) #Calls Input function which calls action funciton, stores last input
+	print(input)
+
+func getInputAndAction(velocity) -> String:
+	if Input.is_action_just_pressed("forward"):
+		input = "forward"
+	elif Input.is_action_just_pressed("backward"):
+		input = "backward"
+	elif Input.is_action_just_pressed("left"):
+		input = "left"
+	elif Input.is_action_just_pressed("right"):
+		input = "right"
+		
+	if input != "":
+		if !Input.is_action_pressed(input): input = ""
+		else: action(velocity)
 	else:
-		if Input.is_action_pressed("forward"):
-			input = "forward"
-			action(velocity, input)
-		elif Input.is_action_pressed("backward"):
-			input = "backward"
-			action(velocity, input)
-		elif Input.is_action_pressed("left"):
-			input = "left"
-			action(velocity, input)
-		elif Input.is_action_pressed("right"):
-			input = "right"
-			action(velocity, input)
+		_animated_sprite.stop()
 	return input
-	
-func action(velocity, input):
+
+func action(velocity):
 	match input:
 		"forward":
 			forward(velocity, Input.is_action_pressed("sprint"))
@@ -39,24 +40,39 @@ func action(velocity, input):
 func forward(velocity, sprint):
 	if sprint:
 		Player.position.y -= velocity * 1.5
+		_animated_sprite.set_speed_scale(1.5)
+		_animated_sprite.play("move_up")
 	else:
-		$"..".position.y -= velocity
+		Player.position.y -= velocity
+		_animated_sprite.set_speed_scale(1)
+		_animated_sprite.play("move_up")
 	
 func backward(velocity, sprint):
 	if sprint:
-		$"..".position.y += velocity * 1.5
+		Player.position.y += velocity * 1.5
+		_animated_sprite.set_speed_scale(1.5)
+		_animated_sprite.play("move_down")
 	else:
-		$"..".position.y += velocity
+		Player.position.y += velocity
+		_animated_sprite.play("move_down")
 		
 func left(velocity, sprint):
 	if sprint:
-		$"..".position.x -= velocity * 1.5
+		Player.position.x -= velocity * 1.5
+		_animated_sprite.set_speed_scale(1.5)
+		_animated_sprite.play("move_left")
 	else:
-		$"..".position.x -= velocity
+		Player.position.x -= velocity
+		_animated_sprite.set_speed_scale(1)
+		_animated_sprite.play("move_left")
 		
 func right(velocity, sprint):
 	if sprint:
-		$"..".position.x += velocity * 1.5
+		Player.position.x += velocity * 1.5
+		_animated_sprite.set_speed_scale(1.5)
+		_animated_sprite.play("move_right")
 	else:
-		$"..".position.x += velocity
+		Player.position.x += velocity
+		_animated_sprite.set_speed_scale(1)
+		_animated_sprite.play("move_right")
 
