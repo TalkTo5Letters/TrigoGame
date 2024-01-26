fb655da7beb4c27d613f11a3246e0925c7a0016d
MyAngelShibuyaKanon <99974252+MyAngelShibuyaKanon@users.noreply.github.com>
2024-01-26 21:33:30 +0800

    better movement system (final)
    pein

--- scenes/characters/player/PlayerMovement.gd
+++ scenes/characters/player/PlayerMovement.gd
@@ -1,90 +1,50 @@
 extends Node2D
 @onready var _animated_sprite = $"../AnimatedSprite2D"
 
-var input: String
 var velocity:Vector2
 @onready var Player = $".." #Sets parent's name to Player
+@export var speed:int = 12500
 
-func _process(delta): 
-	var speed: int = 12000 * delta #4 pixels per second
-	input = getInputAndAction(speed) #Calls Input function which calls action funciton, stores last input
-	
-func getInputAndAction(speed) -> String:
-	if Input.is_action_just_pressed("forward"):
-		input = "forward"
-	elif Input.is_action_just_pressed("backward"):
-		input = "backward"
+var input_buffer = [Vector2.ZERO]
+var input_buffer_readout = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("right"):
+		input_buffer.append(Vector2.RIGHT)
 	elif Input.is_action_just_pressed("left"):
-		input = "left"
-	elif Input.is_action_just_pressed("right"):
-		input = "right"
-		
-	if input != "":
-		if !Input.is_action_pressed(input): input = ""
-		else: action(speed)
-	else:
-		_animated_sprite.stop()
-	return input
+		input_buffer.append(Vector2.LEFT)
+	elif Input.is_action_just_pressed("forward"):
+		input_buffer.append(Vector2.UP)
+	elif Input.is_action_just_pressed("backward"):
+		input_buffer.append(Vector2.DOWN)
 
-func action(speed):
-	match input:
-		"forward":
-			velocity = Vector2(0, -speed)
-			forward(Input.is_action_pressed("sprint"))
-		"backward":
-			velocity = Vector2(0, speed)
-			backward(Input.is_action_pressed("sprint"))
-		"left":
-			velocity = Vector2(-speed, 0)
-			left(Input.is_action_pressed("sprint"))
-		"right":
-			velocity = Vector2(speed, 0)
-			right(Input.is_action_pressed("sprint"))
-	print(velocity)
-		
-func forward(sprint):
-	if sprint:
-		Player.velocity = velocity * 1.5
-		Player.move_and_slide()
-		_animated_sprite.play("move_up")
-	else:
-		Player.velocity = velocity
-		Player.move_and_slide()
-		_animated_sprite.set_speed_scale(1)
-		_animated_sprite.play("move_up")
+	if Input.is_action_just_released("right"):
+		input_buffer.erase(Vector2.RIGHT)
+	elif Input.is_action_just_released("left"):
+		input_buffer.erase(Vector2.LEFT)
+	elif Input.is_action_just_released("forward"):
+		input_buffer.erase(Vector2.UP)
+	elif Input.is_action_just_released("backward"):
+		input_buffer.erase(Vector2.DOWN)
+
+	input_buffer_readout = input_buffer[-1]
 	
-func backward(sprint):
-	if sprint:
-		Player.velocity = velocity * 1.5
-		Player.move_and_slide()
+	match str(input_buffer_readout):
+		"(0, -1)":
+			_animated_sprite.play("move_up")
+		"(0, 1)":
+			_animated_sprite.play("move_down")
+		"(1, 0)":
+			_animated_sprite.play("move_right")
+		"(-1, 0)":
+			_animated_sprite.play("move_left")
+			
+	if input_buffer_readout.x + input_buffer_readout.y == 0: _animated_sprite.stop()
+	if Input.is_action_pressed("sprint"): 
 		_animated_sprite.set_speed_scale(1.5)
-		_animated_sprite.play("move_down")
+		Player.velocity = input_buffer_readout * speed * delta * 1.5
+		Player.move_and_slide()
 	else:
-		Player.velocity = velocity
-		Player.move_and_slide()
-		_animated_sprite.play("move_down")
-		
-func left(sprint):
-	if sprint:
-		Player.velocity = velocity * 1.5
-		Player.move_and_slide()
-		_animated_sprite.set_speed_scale(1.5)
-		_animated_sprite.play("move_left")
-	else:
-		Player.velocity = velocity
-		Player.move_and_slide()
 		_animated_sprite.set_speed_scale(1)
-		_animated_sprite.play("move_left")
-		
-func right(sprint):
-	if sprint:
-		Player.velocity = velocity * 1.5
+		Player.velocity = input_buffer_readout * speed * delta
 		Player.move_and_slide()
-		_animated_sprite.set_speed_scale(1.5)
-		_animated_sprite.play("move_right")
-	else:
-		Player.velocity = velocity
-		Player.move_and_slide()
-		_animated_sprite.set_speed_scale(1)
-		_animated_sprite.play("move_right")
-
