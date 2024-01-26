700d87524fb224f171e51b69d72c625b730dcfd6
MyAngelShibuyaKanon <99974252+MyAngelShibuyaKanon@users.noreply.github.com>
2024-01-26 22:04:41 +0800

    Fix movement bug

--- scenes/characters/player/PlayerMovement.gd
+++ scenes/characters/player/PlayerMovement.gd
@@ -9,6 +9,7 @@ var input_buffer = [Vector2.ZERO]
 var input_buffer_readout = Vector2()
 
 func _physics_process(delta):
+	#checks for keypresses, if found, append to bufferc
 	if Input.is_action_just_pressed("right"):
 		input_buffer.append(Vector2.RIGHT)
 	elif Input.is_action_just_pressed("left"):
@@ -18,17 +19,17 @@ func _physics_process(delta):
 	elif Input.is_action_just_pressed("backward"):
 		input_buffer.append(Vector2.DOWN)
 
-	if Input.is_action_just_released("right"):
+#cancer, checks if any control is not pressed, if it's not, delete it from buffer
+	if !Input.is_action_pressed("right"):
 		input_buffer.erase(Vector2.RIGHT)
-	elif Input.is_action_just_released("left"):
+	if !Input.is_action_pressed("left"):
 		input_buffer.erase(Vector2.LEFT)
-	elif Input.is_action_just_released("forward"):
+	if !Input.is_action_pressed("forward"):
 		input_buffer.erase(Vector2.UP)
-	elif Input.is_action_just_released("backward"):
+	if !Input.is_action_pressed("backward"):
 		input_buffer.erase(Vector2.DOWN)
 
 	input_buffer_readout = input_buffer[-1]
-	
 	match str(input_buffer_readout):
 		"(0, -1)":
 			_animated_sprite.play("move_up")
